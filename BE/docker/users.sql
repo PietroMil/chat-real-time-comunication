90ad5228ecb1e80bda3792d399136e2c669221ee
Pietro Milanese <pietro.milanese@edu.itspiemonte.it>
2023-04-06 17:06:07 +0200

    added email row in users table

--- BE/docker/users.sql
+++ BE/docker/users.sql
@@ -2,6 +2,7 @@ CREATE TABLE
   public.users (
     id serial NOT NULL,
     full_name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
     ws_id character varying(255) NOT NULL UNIQUE,
     last_login timestamp without time zone NOT NULL
   );
