ead10fb8210622294028b66d1f424810b729f401
Pietro Milanese <pietro.milanese@edu.itspiemonte.it>
2023-04-13 14:16:22 +0200

    comment and sql messages added

--- BE/src/websocket.ts
+++ BE/src/websocket.ts
@@ -6,15 +6,18 @@ import { updateUserData } from "./services/user-service";
 const { Pool } = pg;
 const pool = new Pool(config.postgres);
 
+//map to assign websocket connection and user id
 const map = new Map();
 
+//open web socket connection
 export const open = () => {
   const sockserver = new WebSocketServer({ port: 6969 });
   sockserver.on("connection", async (ws, req) => {
+    //take user reference and websocket key from url and request header
     if (req.url && req.headers["sec-websocket-key"]) {
       console.log("New client connected!");
       const id = +req.url.substring(1);
-
+      //update on DB
       const isUpdate = await updateUserData(
         pool,
         id,
